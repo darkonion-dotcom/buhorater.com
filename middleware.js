c4c9f0e4ee182ddc5cd6a1ba7fa5611c7f642990
darkonion.datcom <juanfernadoincognito@gmail.com>
2026-01-19 08:02:26 -0700

    Se desactivo middleware.js

--- middleware.js
+++ middleware.js
@@ -1,18 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
-  const cfCountry = req.headers.get('cf-ipcountry');
-  const vercelCountry = req.headers.get('x-vercel-ip-country');
-  const ip = req.headers.get('x-forwarded-for') || req.ip;
-
-  console.log(`--- INTENTO DE ACCESO ---`);
-  console.log(`Ruta: ${req.nextUrl.pathname}`);
-  console.log(`IP detectada: ${ip}`);
-  console.log(`Pais Cloudflare (cf-ipcountry): ${cfCountry}`);
-  console.log(`Pais Vercel (x-vercel-ip-country): ${vercelCountry}`);
-  console.log(`-------------------------`);
-
-
   return NextResponse.next();
 }
 
